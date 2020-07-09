372547efda3415b9ea81bf934dfa454dfee7092f
Design44 <53436502+Design44@users.noreply.github.com>
2020-07-09 15:27:22 -0400

    Day 4 Done

--- day4/src/App.js
+++ day4/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react"
 import "./App.css"
 
 const WinMessage = () => <div>You Win!</div>
+const TooLow = () => <div>Guess Higher!</div>
+const TooHigh = () => <div>Guess Lower!</div>
+const Cheated = () => <div>...But you cheated :(</div>
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +12,10 @@ class App extends Component {
     this.state = {
       randomValue: Math.floor(Math.random() * 100),
       guess: -1,
-      win: false
+      win: false,
+      highOrLow: 0,
+      showNumber: false,
+      numberShown: false
     }
   }
 
@@ -20,12 +26,69 @@ class App extends Component {
   checkWin = event => {
     event.preventDefault()
     // TODO: check if guess equal to randomValue and update this.state.win
+    if (this.state.guess == this.state.randomValue) {
+      this.setState({win: true})
+    }
   }
+
+  checkHighOrLow = event => {
+    event.preventDefault()
+    if (this.state.guess < this.state.randomValue)  {
+      this.setState({highOrLow: -1})
+    }
+    else if (this.state.guess > this.state.randomValue) {
+      this.setState({highOrLow: 1})
+    }
+    else {
+      this.setState({highOrLow: 0})
+    }
+  }
+
+checkAll = event => {
+  event.preventDefault()
+  this.checkWin(event)
+  this.checkHighOrLow(event)
+  this.NumberEverShown(event)
+}
+
+toggleShowNumber = event => {
+  event.preventDefault()
+  if (this.state.showNumber == false) {
+    this.setState({showNumber: true})
+  }
+  else if (this.state.showNumber == true) {
+    this.setState({showNumber: false})
+  }
+  this.NumberEverShown(event)
+}
+
+NumberEverShown = event => {
+  event.preventDefault()
+  if ((this.state.showNumber == true) & (this.state.win == false)) {
+    this.setState({numberShown: true})
+  }
+}
+
+reset = event => {
+  event.preventDefault()
+  this.setState({randomValue: Math.floor(Math.random() * 100)})
+  this.setState({guess: -1})
+  this.setState({win: false})
+  this.setState({highOrLow: 0})
+  this.setState({showNumber: false})
+  this.setState({numberShown: false})
+}
 
   render() {
     return (
       <div className="App">
-        <div>Current value: {this.state.randomValue}</div>
+        <h1>
+          Guess the Number
+        </h1>
+        <form>
+          <input type="button" value="Reveal Number" onClick={this.toggleShowNumber} />
+        </form>
+        {(this.state.showNumber) && <div>Current value: {this.state.randomValue}</div>}
         <form>
           <label>
             Enter a guess:
@@ -35,9 +98,15 @@ class App extends Component {
               onChange={this.updateGuess}
             />
           </label>
-          <input type="submit" value="Submit" onClick={this.checkWin} />
+          <input type="submit" value="Submit" onClick={this.checkAll} />
         </form>
         {this.state.win && <WinMessage />}
+        {this.state.win && this.state.numberShown && <Cheated />}
+        {(this.state.highOrLow == -1) && <TooLow />}
+        {(this.state.highOrLow == 1) && <TooHigh />}
+        <form>
+          <input type="button" value="Reset" onClick={this.reset} />
+        </form>
       </div>
     )
   }
